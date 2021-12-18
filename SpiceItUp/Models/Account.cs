9a92e8d524770f6164025f5d7a08587c16ce328b
John Melo <joaolucasrmelo@gmail.com>
2021-12-17 22:15:42 -0700

    project initialized

---
+++ SpiceItUp/Models/Account.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace SpiceItUp.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+    }
+}
