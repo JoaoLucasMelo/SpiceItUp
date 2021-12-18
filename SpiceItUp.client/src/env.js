cab32903b28850c582cac5f74d6b1635d896b05e
John Melo <joaolucasrmelo@gmail.com>
2021-12-17 22:22:32 -0700

    Open It Up

--- SpiceItUp.client/src/env.js
+++ SpiceItUp.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'john-codeworks.us.auth0.com'
+export const clientId = 'UQuN3ny3HsAeAStMEfZDLZE85IqdrOg3'
+export const audience = 'https://JohnDev.com'
