9506eae2c13d1dd7f42f13e0a3a5494d6a62e069
John Melo <joaolucasrmelo@gmail.com>
2021-12-20 00:20:27 -0700

    fix

--- SpiceItUp.client/src/components/Steps.vue
+++ SpiceItUp.client/src/components/Steps.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="Steps">
-    <div class="card border-0">
-      <div class="text-center topcard rounded">
-        <p class="font title my-2">Recipe Steps</p>
+    <div class="card border-0" v-if="edit === true">
+      <div
+        class="topcard rounded d-flex justify-content-center align-items-center"
+      >
+        <div class="col-8 text-end">
+          <p class="font title my-2">Recipe Steps</p>
+        </div>
+        <div class="text-end col-4">
+          <i
+            @click="edit = !edit"
+            title="Edit Ingredients"
+            class="mdi pe-3 mdi-18px mdi-pencil selectable1"
+            v-if="recipe.creatorId === account.id"
+          ></i>
+        </div>
       </div>
       <div class="middlecard pt-2">
         <div class="d-flex" v-for="s in steps" :key="s.id">
@@ -41,6 +53,46 @@
         </form>
       </div>
     </div>
+    <div class="card border-0" v-if="edit === false">
+      <div
+        class="topcard rounded d-flex justify-content-center align-items-center"
+      >
+        <div class="col-8 text-end">
+          <p class="font title my-2">Recipe Steps</p>
+        </div>
+        <div class="text-end col-4">
+          <i
+            @click="edit = !edit"
+            title="Edit Ingredients"
+            class="mdi pe-3 mdi-18px mdi-pencil selectable1"
+          ></i>
+        </div>
+      </div>
+      <div class="middlecard pt-2">
+        <div class="d-flex" v-for="s in steps" :key="s.id">
+          <i
+            @click="removeStep(s.id)"
+            title="Remove Ingredient"
+            class="mdi mdi-close text-danger ms-3 selectable1"
+          ></i>
+          <p class="font mx-3 mb-1">{{ s.stepNumber }}.</p>
+          <p class="font mb-1">{{ s.body }}</p>
+        </div>
+      </div>
+      <div
+        class="bottomcard rounded d-flex justify-content-end align-items-center"
+      >
+        <div class="px-2">
+          <p
+            title="Done"
+            @click="edit = !edit"
+            class="font pe-3 f-16 text-primary selectable1"
+          >
+            Done
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -57,13 +109,24 @@ export default {
   },
   setup(props) {
     let add = ref({ ... { recipeId: props.recipe.id } })
+    let edit = ref(true)
     return {
+      edit,
       add,
       steps: computed(() => AppState.activeSteps),
+      account: computed(() => AppState.account),
       async addStep() {
         try {
           await stepsService.addStep(add.value)
           add.value = { ... { recipeId: props.recipe.id } }
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error, 'error')
+        }
+      },
+      async removeStep(id) {
+        try {
+          await stepsService.removeStep(id)
         } catch (error) {
           logger.error(error)
           Pop.toast(error, 'error')
--- SpiceItUp.client/src/services/StepsService.js
+++ SpiceItUp.client/src/services/StepsService.js
@@ -5,7 +5,12 @@ import { api } from "./AxiosService"
 class StepsService {
   async getSteps(id) {
     const res = await api.get("api/recipes/" + id + "/steps")
-    AppState.activeSteps = res.data
+    let steps = AppState.activeSteps
+    steps = res.data
+    for (let i = 0; i < steps.length; i++) {
+      steps[i].stepNumber = i + 1 ;
+    }
+    AppState.activeSteps = steps
     logger.log('steps', AppState.activeSteps)
   }
   async addStep(data) {
@@ -13,6 +18,15 @@ class StepsService {
       const res = await api.post('api/steps', data)
       logger.log('New Step', res.data)
       AppState.activeSteps.push(res.data)
+  }
+  async removeStep(id){
+    const res = await api.delete('api/steps/' + id)
+    logger.log(res.data)
+    let newApp = AppState.activeSteps.filter(s => s.id !== id)
+    for (let i = 0; i < newApp.length; i++) {
+      newApp[i].stepNumber = i + 1 ;
+    }
+    AppState.activeSteps = newApp
   }
 }
 export const stepsService = new StepsService()
